0aa10488b214284c72bb2dabf59943c958d57d92
Linter <hsulinter@gmail.com>
2020-11-18 22:02:13 +0800

    add new file

---
+++ c_to_f.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 18 21:43:45 2020
+
+@author: Linter
+"""
+
+celsius = float(input("請輸入攝氏溫度: "))
+
+fahrenheit = celsius * 9 / 5 + 32
+
+print("華氏溫度為", fahrenheit)
